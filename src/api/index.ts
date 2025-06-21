48d6f6f0ede246a8426702015ea6c84f85b2b8bb
imran-khan-dev <emrankhanniloy@gmail.com>
2025-06-21 21:34:47 +0600

    deploy issue fix

---
+++ src/api/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import serverless from "serverless-http";
+import { bookRoutes } from "../src/app/modules/book/book.route";
+import { borrowRoutes } from "../src/app/modules/borrow/borrow.route";
+import { json } from "body-parser";
+
+const app = express();
+
+app.use(json());
+
+app.use("/api/books", bookRoutes);
+app.use("/api/borrow", borrowRoutes);
+
+export const handler = serverless(app);
